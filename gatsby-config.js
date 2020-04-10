83b1246ad56d0fa508b722aedbbdf0fbd950bd41
Alexander Hadik <alex@transcriptic.com>
2020-04-10 08:47:24 -0700

    Delete another stupid thing

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-66429551-1",
+        trackingId: "UA-66429551-2",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true
       }
